e5bb41c12cc15de1b02c398e0b060dc8052432ac
aakazakov <aakazakovv@yandex.ru>
2020-12-01 22:11:45 +0300

    add methods

--- sandbox/src/dev/fun/arrays/deque/MyDeque.java
+++ sandbox/src/dev/fun/arrays/deque/MyDeque.java
@@ -1,5 +1,7 @@
 package dev.fun.arrays.deque;
 
+import java.util.NoSuchElementException;
+
 public class MyDeque<E> {
 
 	private static final int DEFAULT_CAPACITY = 10;
@@ -8,23 +10,68 @@ public class MyDeque<E> {
 	private int begin;
 	private int end;
 	
+	public Object[] getBase() {
+		return base;
+	}
+	
 	public MyDeque() {
 		base = new Object[DEFAULT_CAPACITY];
 	}
 	
-
 	public MyDeque(int capacity) {
 		if (capacity <= 0)
 			throw new IllegalArgumentException("capacity cannot be " + capacity);
 		base = new Object[capacity];
 	}
 	
+	public void insertRight(E e) throws MyDequeOverflowException {
+		if (isFull())
+			throw new MyDequeOverflowException(base.length);
+		size++;
+		base[end] = e;
+		end = nextIndex(end);
+	}
+	
+	public void insertLeft(E e) throws MyDequeOverflowException {
+		if (isFull())
+			throw new MyDequeOverflowException(base.length);
+		size++;
+		begin = previousIndex(begin);
+		base[begin] = e;
+	}
+	
+	private int nextIndex(int index) {
+		return (index + 1) % base.length;
+	}
+	
+	private int previousIndex(int index) {
+		return index == 0 ? base.length - 1: index - 1;
+	}
+
 	public boolean isEmpty() {
 		return size == 0;
 	}
 	
+	public boolean isFull() {
+		return size == base.length;
+	}
+	
 	public int size() {
 		return size;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public E peakLeft() {
+		if (isEmpty())
+			throw new NoSuchElementException("deque is empty");
+		return (E) base[begin];
+	}
+	
+	@SuppressWarnings("unchecked")
+	public E peakRight() {
+		if (isEmpty())
+			throw new NoSuchElementException("deque is empty");
+		return (E) base[end - 1];
 	}
 	
 }
--- sandbox/test/dev/fun/arrays/deque/MyDequeTest.java
+++ sandbox/test/dev/fun/arrays/deque/MyDequeTest.java
@@ -2,14 +2,86 @@ package dev.fun.arrays.deque;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MyDequeTest {
+	
+	MyDeque<Integer> deque;
+	
+	@BeforeEach
+	void init() {
+		deque = new MyDeque<>();
+	}
 
 	@Test
 	void should_check_emptiness() {
-		MyDeque<Integer> deque = new MyDeque<>();
 		assertTrue(deque.isEmpty());
+	}
+	
+	@Test
+	void check_right_insert() throws MyDequeOverflowException {
+		deque.insertRight(0);
+		deque.insertRight(1);
+		deque.insertRight(2);
+		
+		int size = deque.size();
+		Object[] base = deque.getBase();
+		
+		assertEquals(3, size);
+		assertEquals(0, (Integer) base[0]);
+		assertEquals(1, (Integer) base[1]);
+		assertEquals(2, (Integer) base[2]);
+	}
+	
+	@Test
+	void check_left_insert() throws MyDequeOverflowException {
+		deque.insertLeft(1);
+		deque.insertLeft(2);
+		deque.insertLeft(3);
+		
+		int size = deque.size();
+		Object[] base = deque.getBase();
+		
+		assertEquals(3, size);
+		assertEquals(1, (Integer) base[base.length - 1]);
+		assertEquals(2, (Integer) base[base.length - 2]);
+		assertEquals(3, (Integer) base[base.length - 3]);
+	}
+	
+	@Test
+	void check_left_peak() throws MyDequeOverflowException {
+		deque.insertLeft(1);
+		deque.insertLeft(2);
+		deque.insertLeft(3);
+		
+		Integer actual = deque.peakLeft();
+		
+		assertNotNull(actual);
+		assertEquals(3, actual);
+	}
+	
+	@Test
+	void check_right_peak() throws MyDequeOverflowException {
+		deque.insertRight(0);
+		deque.insertRight(1);
+		deque.insertRight(2);
+		
+		Integer actual = deque.peakRight();
+		
+		assertNotNull(actual);
+		assertEquals(2, actual);
+	}
+	
+	@Test
+	void check_overflow_exception() {
+		Exception e = assertThrows(MyDequeOverflowException.class, () -> {
+			for (int i = 0; i < 11; i++) {
+				deque.insertRight(i);
+			}
+		});
+		
+		assertEquals("deque overflow, max size: 10", e.getMessage());
 	}
 
 }
