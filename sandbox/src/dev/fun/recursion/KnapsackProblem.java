1d1d360fc7c4d16ca46200ec07be795cb6c18192
aakazakov <aakazakovv@yandex.ru>
2020-12-08 17:39:41 +0300

    add another solution

--- sandbox/src/dev/fun/recursion/KnapsackProblem.java
+++ sandbox/src/dev/fun/recursion/KnapsackProblem.java
@@ -10,15 +10,19 @@ public class KnapsackProblem {
 		
 		List<Thing> thingList = new ArrayList<>(
 				Arrays.asList(
-						new Thing(15, 17, "a"), new Thing(7, 17, "b"), new Thing(10, 10, "c"),
-						new Thing(25, 25, "d"), new Thing(5, 5, "e"), new Thing(15, 1, "f"))
+						new Thing(5, 17, "a"), new Thing(7, 17, "b"), new Thing(1, 10, "c"),
+						new Thing(2, 25, "d"), new Thing(3, 5, "e"), new Thing(4, 1, "f"))
 				);
-		thingList.sort((o1, o2) -> o1.getValue() - o2.getValue());
+		thingList.sort((o1, o2) -> o1.getValue() - o2.getValue()); // it's easier with sort )
 		
 		KnapsackSolution solution = new KnapsackSolution();
-		List<Thing> list = solution.greedySolution(thingList, 50, thingList.size() - 1);
+		List<Thing> list = solution.greedySolution(thingList, 15, thingList.size() - 1);
 		
 		System.out.println(list);
+		
+		thingList.sort((o1, o2) -> o1.getVolume() - o2.getVolume()); // it's easier with sort )
+		solution.solution(thingList, 15);
+		
 		
 	}
 }
--- sandbox/src/dev/fun/recursion/KnapsackSolution.java
+++ sandbox/src/dev/fun/recursion/KnapsackSolution.java
@@ -2,8 +2,40 @@ package dev.fun.recursion;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class KnapsackSolution {
+	
+	private class Entry {
+		private List<Thing> things;
+		private int totalValue;
+		private int totalVolume;
+		
+		List<Thing> getThings() { 
+			return things.stream().filter(t -> t.getValue() != 0).collect(Collectors.toList()); 
+		}
+		
+		int getTotalValue() { return totalValue; }
+		
+		int getTotalVolume() { return totalVolume; }
+		
+		Entry(Thing t) {
+			things = new ArrayList<>();
+			add(t);
+		}
+		
+		void add(Thing t) {
+			if (!things.contains(t)) {
+				things.add(t);
+				totalValue += t.getValue();
+				totalVolume += t.getVolume();
+			}
+		}
+		
+		void add(Entry e) {
+			e.getThings().forEach(this::add);
+		}
+	}
 	
 	private List<Thing> list;
 	
@@ -23,5 +55,67 @@ public class KnapsackSolution {
 		}
 		return list;
 	}
+
+	// Dynamic programming method ))
+	public void solution(List<Thing> things, int knapsackVolume) {
+		int minVol = things.get(0).getVolume(); // Minimum volume
+		int parts = knapsackVolume / minVol; // Quantity of minimum volumes
+		
+		Entry[][] matrix = new Entry[things.size() + 1][parts + 1];
+		
+		// If there is no things
+		for (int part = 0; part < parts + 1; part++) {
+			matrix[0][part] = new Entry(new Thing(0, 0, "null"));
+		}
+		
+		// If there is no capacity
+		for (int thing = 0; thing < things.size() + 1; thing++) {
+			matrix[thing][0] = new Entry(new Thing(0, 0, "null"));
+		}
+		
+		run(things, matrix, 1, 1, 0);
+		
+		System.out.println(matrix[things.size()][parts].getThings());
+		
+	}
 	
+	/**
+	 * Fills the 2D array
+	 * @param things list of things i want to put in the knapsack
+	 * @param matrix 2D array of possible solutions for different volumes
+	 * @param row row number in matrix
+	 * @param col column number in matrix (shows volume)
+	 * @param index thing number in the list
+	 */
+	private void run(List<Thing> things, Entry[][] matrix, int row, int col, int index) {
+		if (row >= matrix.length || col >= matrix[0].length) {
+			return;
+		}
+		
+		for (int vol = col; vol < matrix[row].length; vol++) {
+			
+			Entry current = new Entry(things.get(index));
+			int currentVolume = current.getTotalVolume();
+			
+			Entry previous = matrix[row - 1][vol];
+			
+			if (currentVolume <= vol) {
+				int freeVolume = vol - currentVolume;				
+				current.add(matrix[row -1][freeVolume]);
+			} else {
+				matrix[row][vol] = previous;
+			}
+			
+			if (current.getTotalValue() > previous.getTotalValue()) {
+				matrix[row][vol] = current;
+			} else {
+				matrix[row][vol] = previous;
+			}
+			
+		}
+		
+		
+		run(things, matrix, ++row, col, ++index);
+	}
+ 	
 }
