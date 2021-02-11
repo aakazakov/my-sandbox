4a6601cc2077b049718694906c58d8677371ed0e
nito nise <aakazakovv@yandex.ru>
2021-02-11 16:20:09 +0300

    add  timetable

--- sandbox/src/dev/fun/sql/cinema.sql
+++ sandbox/src/dev/fun/sql/cinema.sql
@@ -15,7 +15,8 @@ DROP TABLE IF EXISTS sessions_tbl;
 CREATE TABLE sessions_tbl (
   id_fld SERIAL PRIMARY KEY,
   start_fld TIME NOT NULL,
-  finish_tbl TIME NOT NULL
+  finish_tbl TIME NOT NULL,
+  price_coefficient_fld SMALLINT NOT NULL
 );
 
 DROP TABLE IF EXISTS types_of_days_tbl;
@@ -23,4 +24,26 @@ CREATE TABLE types_of_days_tbl (
   id_fld SERIAL PRIMARY KEY,
   type_fld VARCHAR(32) NOT NULL,
   price_coefficient_fld SMALLINT NOT NULL
-)
+);
+
+DROP TABLE IF EXISTS timetable_tbl;
+CREATE TABLE timetable_tbl (
+  type_of_day_id_fld INT NOT NULL,
+  session_id_fld INT NOT NULL,
+  film_id_fld INT NOT NULL,
+  price INT NOT NULL,
+  CONSTRAINT timetable_pkey
+    PRIMARY KEY(type_of_day_id_fld, session_id_fld, film_id_fld),
+  CONSTRAINT fk_type_of_day
+    FOREIGN KEY(type_of_day_id_fld)
+      REFERENCES types_of_days_tbl(id_fld)
+        ON DELETE CASCADE,
+  CONSTRAINT fk_session
+    FOREIGN KEY(session_id_fld)
+      REFERENCES sessions_tbl(id_fld)
+        ON DELETE CASCADE,
+  CONSTRAINT fk_film
+    FOREIGN KEY(film_id_fld)
+      REFERENCES films_tbl(id_fld)
+        ON DELETE CASCADE
+);
