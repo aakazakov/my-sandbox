f9d522a20527eaef03042510f091a9db84561bce
nito nise <aakazakovv@yandex.ru>
2021-02-11 09:48:39 +0300

    fix

--- sandbox/src/dev/fun/concurrent/Counter.java
+++ sandbox/src/dev/fun/concurrent/Counter.java
@@ -4,21 +4,26 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Counter {
 	
-	private final ReentrantLock lock = new ReentrantLock();
+	private final ReentrantLock lock;
 	
 	private int value;
 	
+	public int getValue() {
+		return value;
+	}
+	
+	public Counter() {
+		lock = new ReentrantLock();
+		value = 0;
+	}
+	
 	public void add(int n) {
-		lock.lock();
-		value += n;
-		System.out.println(value);
 		try {
-			Thread.sleep(1500);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-			Thread.currentThread().interrupt();
+			lock.lock();
+			value += n;
+		} finally {
+			lock.unlock();
 		}
-		lock.unlock();
 	}
 
 }
