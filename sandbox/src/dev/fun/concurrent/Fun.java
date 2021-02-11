cb1d2295c99a0bc72482f040b155c04fc42edcc6
nito nise <aakazakovv@yandex.ru>
2021-02-11 09:49:27 +0300

    format

--- sandbox/src/dev/fun/concurrent/Fun.java
+++ sandbox/src/dev/fun/concurrent/Fun.java
@@ -18,16 +18,16 @@ public class Fun {
 			}
 		}).start();
 		
-//			Counter counter = new Counter();
-//		
-//			new Thread(() -> counter.add(2)).start();
-//			new Thread(() -> counter.add(-2)).start();
-//			new Thread(() -> counter.add(5)).start();
-//			new Thread(() -> counter.add(-5)).start();
-//			new Thread(() -> counter.add(1)).start();
-//			new Thread(() -> counter.add(-1)).start();
-//			new Thread(() -> counter.add(3)).start();
-//			new Thread(() -> counter.add(-3)).start();
+		Counter counter = new Counter();
+	
+		new Thread(() -> counter.add(2)).start();
+		new Thread(() -> counter.add(-2)).start();
+		new Thread(() -> counter.add(5)).start();
+		new Thread(() -> counter.add(-5)).start();
+		new Thread(() -> counter.add(1)).start();
+		new Thread(() -> counter.add(-1)).start();
+		new Thread(() -> counter.add(3)).start();
+		new Thread(() -> counter.add(-3)).start();
 		
 	}
 	
