eb4e2c8acef83c02d498469512b47928dc762939
nito nise <aakazakovv@yandex.ru>
2021-02-12 12:20:20 +0300

    fill data

--- sandbox/src/dev/fun/sql/cinema.sql
+++ sandbox/src/dev/fun/sql/cinema.sql
@@ -8,7 +8,7 @@ CREATE TABLE films_tbl (
   id_fld SERIAL PRIMARY KEY,
   title_fld VARCHAR(255) NOT NULL,
   duration_fld SMALLINT NOT NULL,
-  price_fld INT NOT NULL
+  base_price_fld INT NOT NULL
 );
 
 DROP TABLE IF EXISTS sessions_tbl;
@@ -31,7 +31,6 @@ CREATE TABLE timetable_tbl (
   type_of_day_id_fld INT NOT NULL,
   session_id_fld INT NOT NULL,
   film_id_fld INT NOT NULL,
-  price INT NOT NULL,
   CONSTRAINT timetable_pkey
     PRIMARY KEY(type_of_day_id_fld, session_id_fld, film_id_fld),
   CONSTRAINT fk_type_of_day
@@ -47,3 +46,27 @@ CREATE TABLE timetable_tbl (
       REFERENCES films_tbl(id_fld)
         ON DELETE CASCADE
 );
+
+INSERT INTO films_tbl (title_fld, duration_fld, base_price_fld) VALUES ('film1', 60, 50);
+INSERT INTO films_tbl (title_fld, duration_fld, base_price_fld) VALUES ('film2', 90, 60);
+INSERT INTO films_tbl (title_fld, duration_fld, base_price_fld) VALUES ('film3', 120, 70);
+
+INSERT INTO sessions_tbl (start_fld, finish_tbl, price_coefficient_fld) VALUES ('07:00', '12:00', 2);
+INSERT INTO sessions_tbl (start_fld, finish_tbl, price_coefficient_fld) VALUES ('12:30', '17:30', 2);
+INSERT INTO sessions_tbl (start_fld, finish_tbl, price_coefficient_fld) VALUES ('18:00', '23:00', 3);
+INSERT INTO sessions_tbl (start_fld, finish_tbl, price_coefficient_fld) VALUES ('23:30', '04:30', 1);
+
+INSERT INTO types_of_days_tbl (type_fld, price_coefficient_fld) VALUES ('weekday', 1);
+INSERT INTO types_of_days_tbl (type_fld, price_coefficient_fld) VALUES ('day_off', 2);
+INSERT INTO types_of_days_tbl (type_fld, price_coefficient_fld) VALUES ('holiday', 3);
+
+
+INSERT INTO timetable_tbl (type_of_day_id_fld, session_id_fld, film_id_fld) VALUES (1, 1, 1);
+INSERT INTO timetable_tbl (type_of_day_id_fld, session_id_fld, film_id_fld) VALUES (1, 1, 2);
+INSERT INTO timetable_tbl (type_of_day_id_fld, session_id_fld, film_id_fld) VALUES (1, 1, 3);
+INSERT INTO timetable_tbl (type_of_day_id_fld, session_id_fld, film_id_fld) VALUES (1, 2, 1);
+INSERT INTO timetable_tbl (type_of_day_id_fld, session_id_fld, film_id_fld) VALUES (1, 2, 2);
+INSERT INTO timetable_tbl (type_of_day_id_fld, session_id_fld, film_id_fld) VALUES (1, 3, 1);
+INSERT INTO timetable_tbl (type_of_day_id_fld, session_id_fld, film_id_fld) VALUES (1, 3, 2);
+INSERT INTO timetable_tbl (type_of_day_id_fld, session_id_fld, film_id_fld) VALUES (1, 3, 3);
+INSERT INTO timetable_tbl (type_of_day_id_fld, session_id_fld, film_id_fld) VALUES (1, 4, 3);
